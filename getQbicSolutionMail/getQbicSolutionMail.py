d02e9f4507f8e7139ac80c796f74265075864e7a
Sascha Dodenhöft <30420781+ds1978@users.noreply.github.com>
2023-02-23 07:12:16 -0800

    Multiple qiner logs (#1)
    * Stage
    
    * support for mulltiple logfiles added
    
    * added support for multiple logs
    
    added support for multiple logs; impotant if running multiple threads on miner

--- getQbicSolutionMail/getQbicSolutionMail.py
+++ getQbicSolutionMail/getQbicSolutionMail.py
@@ -1,21 +1,20 @@
 import os
+import shutil
 import json
 import smtplib, ssl
 import socket
+import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-scriptversion = "1.2.2"
-scriptdate="20230220"
+scriptversion = "1.2.3"
+scriptdate="20230223"
 basepath = "/opt/qiner/mailreport"
 hostname = socket.gethostname()
 
 with open(basepath +"/config.json") as config:
     data = json.load(config)
 
-logfilename = data['other']['logfilename'] 
-logfilepath = logfilename
-lastsolutionfilename = basepath + "/lastsolution.json"
-lastsolutionfilepath = lastsolutionfilename
+logfilenames = data['other']['logfilename'] 
 
 emailport = data['email']['emailport']  # For SSL
 emailusername = data['email']['emailusername'] 
@@ -25,52 +24,59 @@ emailfromname = data['email']['emailfromname']
 emailsender = data['email']['emailsender'] 
 emailreceiver = data['email']['emailreceiver'] 
 
-with open(logfilepath, 'r') as f:
-    try:  # catch OSError in case of a one line file 
-        lines = f.readlines()
-        second_last_line = lines[-2]    
-    except OSError:
-        print('error')
+for logfilename in logfilenames:
+    logfilefile= logfilename.split('/')[-1].replace('.sh.log','')
+    lastsolutionfilename = basepath + "/lastsolution_"+logfilefile+'.json'
     
-    last_line = second_last_line
-    last_line_split = last_line.split('|')
-    if "solutions" in last_line_split[3]:
-        with open(lastsolutionfilepath, 'r') as openfile:
-            # Reading from json file
-            json_object_in = json.load(openfile)
-            solutionsfound_in = json_object_in.get('solutionsfound') 
-        solutionsfounddate_new = last_line_split[1].strip(' ')
-        solutionsfoundcount_new = last_line_split[3].strip(' ').strip(' solutions')
-        its_speed = last_line_split[2].strip(' ')
-        dictionary = {
-            "date": last_line_split[1].strip(' '),
-            "solutionsfound": solutionsfoundcount_new
-        }
+    if not os.path.isfile(lastsolutionfilename):
+        shutil.copyfile(basepath +"/lastsolution_base.json", lastsolutionfilename)
 
-        if int(solutionsfoundcount_new) > int(solutionsfound_in):
-            # Serializing json
-            json_object = json.dumps(dictionary, indent=4)
-            # Writing to sample.json
-            with open(lastsolutionfilepath, "w") as outfile:
-                outfile.write(json_object)
-                outfile.close()
-            # Create a secure SSL context
-            emailmessage = MIMEMultipart("alternative")
-            emailmessage["Subject"] = "qbic solution found:" + hostname
-            emailmessage["From"] = emailsender
-            emailmessage["To"] = emailreceiver
-            emailmessagetxt = 'Scriptversion: ' + scriptversion + '\nHost: ' + hostname + '\nDate (utc): ' + str(solutionsfounddate_new) + "\nCount: " + str(solutionsfoundcount_new)+  "\nit/s: " + str(its_speed) 
-            
-            part1 = MIMEText(emailmessagetxt, "plain")
-            #part2 = MIMEText(emailmessagetxt, "html")
+    with open(logfilename, 'r') as f:
+        try:  # catch OSError in case of a one line file 
+            lines = f.readlines()
+            second_last_line = lines[-2]    
+        except OSError:
+            print('error')
+        
+        last_line = second_last_line
+        last_line_split = last_line.split('|')
+        if "solutions" in last_line_split[3]:
+            with open(lastsolutionfilename, 'r') as openfile:
+                # Reading from json file
+                json_object_in = json.load(openfile)
+                solutionsfound_in = json_object_in.get('solutionsfound') 
+            solutionsfounddate_new = last_line_split[1].strip(' ')
+            solutionsfoundcount_new = last_line_split[3].strip(' ').strip(' solutions')
+            its_speed = last_line_split[2].strip(' ')
+            dictionary = {
+                "date": last_line_split[1].strip(' '),
+                "solutionsfound": solutionsfoundcount_new
+            }
 
-            emailmessage.attach(part1)
+            if int(solutionsfoundcount_new) > int(solutionsfound_in):
+                # Serializing json
+                json_object = json.dumps(dictionary, indent=4)
+                # Writing to sample.json
+                with open(lastsolutionfilename, "w") as outfile:
+                    outfile.write(json_object)
+                    outfile.close()
+                # Create a secure SSL context
+                emailmessage = MIMEMultipart("alternative")
+                emailmessage["Subject"] = "qbic solution found:" + hostname
+                emailmessage["From"] = emailsender
+                emailmessage["To"] = emailreceiver
+                emailmessagetxt = 'Scriptversion: ' + scriptversion + '\nHost: ' + hostname + '\nDate (utc): ' + str(solutionsfounddate_new) + "\nCount: " + str(solutionsfoundcount_new)+  "\nit/s: " + str(its_speed) 
+                
+                part1 = MIMEText(emailmessagetxt, "plain")
+                #part2 = MIMEText(emailmessagetxt, "html")
+
+                emailmessage.attach(part1)
 
 
-            context = ssl.create_default_context()
-            with smtplib.SMTP(emailserver, emailport) as server:
-                server.ehlo()
-                server.starttls(context=context)
-                server.login(emailusername, emailpassword)
-                server.sendmail(emailsender, emailreceiver, emailmessage.as_string())
+                context = ssl.create_default_context()
+                with smtplib.SMTP(emailserver, emailport) as server:
+                    server.ehlo()
+                    server.starttls(context=context)
+                    server.login(emailusername, emailpassword)
+                    server.sendmail(emailsender, emailreceiver, emailmessage.as_string())
 
